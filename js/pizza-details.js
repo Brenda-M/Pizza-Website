0b22720d1c4698ed3b043f6dedbd35cfe902bf5d
Brenda-M <brendanjuguna1@gmail.com>
2020-03-01 17:25:28 +0300

    create prototypes for delivery price, total order, and pizza prices

--- js/pizza-details.js
+++ js/pizza-details.js
@@ -6,5 +6,18 @@ $(document).ready(function(){
         this.quantity = quantity;
     }
 
+    Pizza.prototype.deliveryPrice = 150;
+
+    Pizza.prototype.orderTotal = 0;
+
+    Pizza.prototype.pizzaSizePrices = [
+        { size: "small", price: 550 }, 
+        { size: "medium", price: 850 },
+        { size: "large", price: 1050 },
+        { size: "extralarge", price: 1200 },
+    ];
+
+    
+
 
 })
