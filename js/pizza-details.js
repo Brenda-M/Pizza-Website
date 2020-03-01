d22e279e52428bf8e9f2b7d5f56a713a012ec113
Brenda-M <brendanjuguna1@gmail.com>
2020-03-01 17:20:18 +0300

    create objects for pizza function

--- js/pizza-details.js
+++ js/pizza-details.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    function Pizza (size, crustType, toppings, quantity, isDelivery)
+    function Pizza (size, crustType, toppings, quantity, isDelivery){
+        this.size = size;
+        this.crustType = crustType;
+        this.toppings = toppings;
+        this.quantity = quantity;
+    }
+
+
 })
